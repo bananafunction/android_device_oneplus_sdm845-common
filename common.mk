b7e7a92e9a519b276f4166947f1620e30204813e
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-12-09 19:54:15 -0800

    sdm845-common: cleanup: Put that thing back where it came from
    ...or so help me...

--- common.mk
+++ common.mk
@@ -145,9 +145,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.livedisplay@2.0-service.oneplus_sdm845
 
-PRODUCT_PACKAGES += \
-    libavservices_minijail
-
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
@@ -224,7 +221,9 @@ PRODUCT_PACKAGES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libnl
+    libnl \
+    libavservices_minijail
+
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
