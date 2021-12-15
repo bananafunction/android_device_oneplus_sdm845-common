ccdddba7dd852829f884679f9a3a0c96445c9610
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-12-15 14:38:37 -0800

    sdm845-common: packages: Include prebuilt OnePlus Camera
    Includes adaptations of Vachounet's "set client package name"
    and AnierinB's "Add OnePlus Camera support!" commits.
    
    Co-authored-by: DennySPB <dennyspb@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: AnierinB <anierin@evolution-x.org>

--- common.mk
+++ common.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.devicesettings.rc \
+    init.opcamera.rc \
     init.qcom.rc \
     init.recovery.qcom.rc \
     init.smartcharging.rc \
@@ -171,6 +172,12 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     oneplus-fwk
+
+# OnePlus Camera
+$(call inherit-product-if-exists, packages/apps/OnePlusCamera/config.mk)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/vendor_overlay/android.hardware.camera.provider@2.4-service.rc:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/android.hardware.camera.provider@2.4-service.rc
 
 # Power
 PRODUCT_PACKAGES += \
